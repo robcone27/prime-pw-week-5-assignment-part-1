3ce3916b63b6b762a63f86f0c2dab94575d2ac4e
Rob Cone <robcone86@gmail.com>
2021-09-18 15:14:59 -0500

    repoupdate 9.18

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -27,6 +27,9 @@ console.log('A little about me:', me);
 let fullName = me.firstName + me.lastName;
 console.log(fullName);
 
+//added the first name and last name object. i tried adding '' in there
+//get a space  in between 'me.firstName + me.lastName' and some other ideas but could
+//not get the space
 
 /* 3. Nested arrays
   - Console.log your first favorite food
@@ -35,6 +38,7 @@ console.log(fullName);
 console.log('first favorite food:', me.favThreeFoods[0] );
 console.log('last favorite food:', me.favThreeFoods[2] );
 
+// logged the array index to show my first and last fav foods
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -45,6 +49,8 @@ console.log('last favorite food:', me.favThreeFoods[2] );
 me.shoeCount++;
 console.log('updated shoe count:', me.shoeCount);
 
+// from previous lessons i knew adding the ++ operator would add 1 more
+// to what ever number variable
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -52,10 +58,9 @@ console.log('updated shoe count:', me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+console.log('adding a new property to my "me" object');
 me.favoriteColor = 'black';
 console.log(me);
 
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
+// through this weeks lessons i know you could add another variable to your
+//existing object by adding the above code.
